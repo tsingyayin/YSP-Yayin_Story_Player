dcc2817ad63b465c8b2a5e588ed3d24486d7b163
JUNYI HE <77230422+tsingyayin@users.noreply.github.com>
2021-05-08 22:56:56 +0800

    Add files via upload

--- Playercore/core/core0_3_5_U.py
+++ Playercore/core/core0_3_5_U.py
@@ -1,9 +1,13 @@
 #这个文件是解释器核心
+#这个是给UI用的版本
 import time as tm
+import sys
 from langcontrol import *
 from global_value import warnline,texterrorline,numseterrorline,formatwarnline
+from PyQt5.QtCore import *
 
-def SPOL(files,Storyname):
+
+def SPOL(self,files,Storyname):
   global warnline,texterrorline,numseterrorline,formatwarnline
     #跨行注释状态确认
   textend=0
@@ -50,13 +54,14 @@ def SPOL(files,Storyname):
             if type(eval(bgsetlst[2]))!=int or (not 0<=eval(bgsetlst[2])<=3):raise Exception
             if (type(eval(bgsetlst[3]))!=int and type(eval(bgsetlst[3]))!=float) or 0>eval(bgsetlst[3]):raise Exception
             print(round(tm.time()-timestart,2),msg("Second"))
-            print("#################\n"+msg("Bgp_Setting_Info").format(bgsetlst[0],bgdisplaymode[bgsetlst[1]],bgeffectmode[bgsetlst[2]],bgsetlst[3]))
-            print("#################\n")
+            self.can_update_bg.emit(bgsetlst)
+            self.pause()
         except Exception:
             numseterrorline+=[[linecount,Storyname,line[:-1]]]
             continue
         else:
             None
+        
 
     #提取音频控制器，不标准的输入用默认值填充
     #音频控制器的参数是音频名称和音频音量
@@ -113,7 +118,6 @@ def SPOL(files,Storyname):
         BGblackcount=0   #用于控制是否需要渐变黑遮罩
         BGblack=1
         #分离每个人的立绘信息和语句，并把立绘信息拆分成人名、表情、翻转和淡入、淡出、说话情况
-        charapic=[]
         try:
             for i in inforaw:
                 charapicsetcount=len(i.split(":")[0].split("/"))
@@ -144,10 +148,8 @@ def SPOL(files,Storyname):
         print(round(tm.time()-timestart,2),msg("Second"))
         if BGblackcount==2 and charanum==2:
             BGblack=0
-            print("无遮罩",end="")
         else:
             BGblack=1
-            print("有遮罩",end="")
 
         #判定立绘明暗状态
         #如果场上只有一人，无论说话与否均为明亮
@@ -166,41 +168,9 @@ def SPOL(files,Storyname):
             elif charawords[0][1]=="" and charawords[1][1]!="":
                 charapic[0][5]="(暗，沉默)"
                 charapic[1][5]="(亮，讲述)"
-
-        #输出立绘
-        for i in charapic:
-            if i[0]!="" and  charanum != 1:       #人物个数不是一个且不是空立绘的时候
-                if i[2]=="0":
-                    print("\t\t"+msg("Chara_Pic_Setting_Info").format(i[0]+"_"+i[1],i[5],i[3],i[4]),end="")
-                elif i[2]=="1":
-                    print("\t\t"+msg("Chara_Pic_Setting_Info_R").format(i[0]+"_"+i[1],i[5],i[3],i[4]),end="")
-            elif i[0]!="" and charanum ==1:     #人物个数只有一个且不是空立绘的时候
-                if i[2]=="0":
-                    print("\t\t\t\t\t"+msg("Chara_Pic_Setting_Info").format(i[0]+"_"+i[1],i[5],i[3],i[4]),end="")
-                elif i[2]=="1":
-                    print("\t\t\t\t\t"+msg("Chara_Pic_Setting_Info_R").format(i[0]+"_"+i[1],i[5],i[3],i[4]),end="")
-            else:                                               #使用空立绘当做对齐手段或是旁白的时候
-                print("\t\t\t\t\t\t\t",end="")
-
-        print("\n")
-
-        #按文本控制器指示输出文本，区分为旁白型和对话型
-        for i in charawords:
-            if i[0]=="" and charanum==1:
-                print("{: ^10}\t".format(""),end="")#没有说话人，旁白型
-                for words in i[1]:
-                    print(words,end="")
-                    tm.sleep(eval(wordset[0]))
-            elif i[0]!="" and i[1]!="":
-                print("{: ^10}\t".format(i[0]),end="")#有说话人在说话，对话型
-                for words in i[1]:
-                    print(words,end="")
-                    tm.sleep(eval(wordset[0]))
-            #有说话人但是沉默则直接略过
-        print("\n")
-        tm.sleep(eval(wordset[1]))
-        print("\n")
-
+        self.can_update_chara.emit(charapic,charawords,wordset,charanum,BGblack)
+        self.pause()
+        
     #自由文本控制器
     elif line[0:3]==">^>":
         #首先判断是否符合要求，如若不符合要求则跳过这一行并录入错误传递列表
@@ -222,6 +192,7 @@ def SPOL(files,Storyname):
         try:
             if (type(eval(wordset[0]))!=int and type(eval(wordset[0]))!=float) or eval(wordset[0])<0:raise Exception
             if (type(eval(wordset[1]))!=int and type(eval(wordset[1]))!=float) or eval(wordset[1])<0:raise Exception
+            testset=[]
             #提取文本内容
             inforaw=[line[3:line.index(":")],line[line.index(":")+1:-1]]
             textsetcount=len(inforaw[0].split("/"))
@@ -291,185 +262,3 @@ def SPOL(files,Storyname):
       return Usrbranchinput
   else:
       return 0
-
-
-def SPOL_s(line):
-    textmode={"L":msg("Freedom_Text_Mode_L"),"M":msg("Freedom_Text_Mode_L"),"R":msg("Freedom_Text_Mode_L")}
-    #剧情文本输出的解释器，只有文本输出控制器。
-    if line=="\n":
-        None
-    #讲述控制器单行版
-    elif line[0:3]==">>>":
-        if (":" not in line) or line.count(">>>")>2 :
-            print("sysinfo→"+msg("Single_Mode_Syntax_Error"))
-            return
-        #首先提取文本控制器，若未发现则按默认值填充
-        wordset=["",""]
-        if line[-2]==")":
-            wordsetcount=len(line[line.rindex("(")+1:-2].split(","))
-            if wordsetcount ==1:
-                wordset=[line[line.rindex("(")+1:-2],"1.5"]
-            else:
-                wordset=[line[line.rindex("(")+1:-2].split(",")[0],line[line.rindex("(")+1:-2].split(",")[1]]
-            line=line[0:line.rindex("(")+1]       #方便下面处理，将文本控制器从字符串中删去
-        #填充文本控制器空位
-        if wordset[0]=="":wordset[0]="0.1"
-        if wordset[1]=="":wordset[1]="1.5"
-        try:
-            if (type(eval(wordset[0]))!=int and type(eval(wordset[0]))!=float) or eval(wordset[0])<0:raise Exception
-            if (type(eval(wordset[1]))!=int and type(eval(wordset[1]))!=float) or eval(wordset[1])<0:raise Exception
-        except Exception:
-            print("sysinfo→"+msg("Single_Mode_Num_Error"))
-            return
-        else:
-            None
-
-        #按下划线分割说话人和对应语句
-        #下一句split从1开始是因为前面有一个空字符需要舍去
-        inforaw=line[0:-1].split(">>>")[1:]      
-        charanum=len(inforaw)   #获取人物个数
-        charawords=[]       #用于存储人物姓名和对应语句
-        charapic=[]           #用于存储立绘和说话状态——即立绘是否需要淡色
-        BGblackcount=0   #用于控制是否需要渐变黑遮罩
-        BGblack=1
-        #分离每个人的立绘信息和语句，并把立绘信息拆分成人名、表情、翻转和淡入、淡出、说话情况
-        charapic=[]
-        try:
-            for i in inforaw:
-                charapicsetcount=len(i.split(":")[0].split("/"))
-                if charapicsetcount>5:raise Exception
-                charapic+=[i.split(":")[0].split("/")+[""]*(6-charapicsetcount)]
-                if charapic[-1][1]=="":charapic[-1][1]=""
-                if charapic[-1][2]=="":charapic[-1][2]="0"
-                if charapic[-1][3]=="":charapic[-1][3]="0.5"
-                if charapic[-1][4]=="":charapic[-1][4]="0.5"
-                #把人物名称和所说的话传入字段
-                if len(i.split(":"))==2:
-                    charawords+=[[i.split(":")[0].split("/")[0],i.split(":")[1]]]
-                elif len(i.split(":"))==3:
-                    charawords+=[[i.split(":")[1],i.split(":")[2]]]
-                #对于不合要求的设置抛出异常
-            if (charapic[-1][2]!="0" and charapic[-1][2]!="1"):raise Exception
-            if (type(eval(charapic[-1][3]))!=int and type(eval(charapic[-1][3]))!=float) or eval(charapic[-1][3])<0:raise Exception
-            if (type(eval(charapic[-1][4]))!=int and type(eval(charapic[-1][4]))!=float) or eval(charapic[-1][4])<0:raise Exception
-        except Exception:
-            print("sysinfo→"+msg("Single_Mode_Num_Error"))
-            return
-        else:
-            None
-
-        #全空计数和全空个数计数,用于确定是否需要渐变黑遮罩
-        for i in charapic:  
-            if i[0]=="":BGblackcount+=1
-        if BGblackcount==2 and charanum==2:
-            BGblack=0
-            print("无遮罩",end="")
-        else:
-            BGblack=1
-            print("有遮罩",end="")
-
-        #判定立绘明暗状态
-        #如果场上只有一人，无论说话与否均为明亮
-        #如果场上有两人，只有在同时沉默的时候均为明亮，否则沉默者暗
-        if charanum==1:
-            if charawords[0][1]=="":
-                charapic[0][5]="(亮，沉默)"
-            elif charawords[0][1]!="":
-                charapic[0][5]="(亮，讲述)"
-        elif charanum==2:
-            if charawords[0][1]==charawords[1][1]=="":
-                charapic[0][5]=charapic[1][5]="(亮，沉默)"
-            elif charawords[0][1]!="" and charawords[1][1]=="":
-                charapic[0][5]="(亮，讲述)"
-                charapic[1][5]="(暗，沉默)"
-            elif charawords[0][1]=="" and charawords[1][1]!="":
-                charapic[0][5]="(暗，沉默)"
-                charapic[1][5]="(亮，讲述)"
-
-        #输出立绘
-        for i in charapic:
-            if i[0]!="" and  charanum != 1:       #人物个数不是一个且不是空立绘的时候
-                if i[2]=="0":
-                    print("\t\t"+msg("Chara_Pic_Setting_Info").format(i[0]+"_"+i[1],i[5],i[3],i[4]),end="")
-                elif i[2]=="1":
-                    print("\t\t"+msg("Chara_Pic_Setting_Info_R").format(i[0]+"_"+i[1],i[5],i[3],i[4]),end="")
-            elif i[0]!="" and charanum ==1:     #人物个数只有一个且不是空立绘的时候
-                if i[2]=="0":
-                    print("\t\t\t\t\t"+msg("Chara_Pic_Setting_Info").format(i[0]+"_"+i[1],i[5],i[3],i[4]),end="")
-                elif i[2]=="1":
-                    print("\t\t\t\t\t"+msg("Chara_Pic_Setting_Info_R").format(i[0]+"_"+i[1],i[5],i[3],i[4]),end="")
-            else:                                               #使用空立绘当做对齐手段或是旁白的时候
-                print("\t\t\t\t\t\t\t",end="")
-
-        print("\n")
-
-        #按文本控制器指示输出文本，区分为旁白型和对话型
-        for i in charawords:
-            if i[0]=="" and charanum==1:
-                print("{: ^10}\t".format(""),end="")#没有说话人，旁白型
-                for words in i[1]:
-                    print(words,end="")
-                    tm.sleep(eval(wordset[0]))
-            elif i[0]!="" and i[1]!="":
-                print("{: ^10}\t".format(i[0]),end="")#有说话人在说话，对话型
-                for words in i[1]:
-                    print(words,end="")
-                    tm.sleep(eval(wordset[0]))
-            #有说话人但是沉默则直接略过
-        print("\n")
-        tm.sleep(eval(wordset[1]))
-
-    #自由文本控制器单行版
-    elif line[0:3]==">^>":
-        #首先判断是否符合要求，如若不符合要求则跳过这一行并录入错误传递列表
-        if (":" not in line) or line.count(">^>")>1 :
-            print("sysinfo→"+msg("Single_Mode_Syntax_Error"))
-            return
-        #首先提取文本控制器，若未发现则按默认值填充
-        wordset=["",""]
-        if line[-2]==")":
-            wordsetcount=len(line[line.rindex("(")+1:-2].split(","))
-            if wordsetcount ==1:
-                wordset=[line[line.rindex("(")+1:-2],"1.5"]
-            else:
-                wordset=[line[line.rindex("(")+1:-2].split(",")[0],line[line.rindex("(")+1:-2].split(",")[1]]
-            line=line[0:line.rindex("(")+1]       #方便下面处理，将文本控制器从字符串中删去
-        #填充文本控制器空位
-        if wordset[0]=="":wordset[0]="0.1"
-        if wordset[1]=="":wordset[1]="1.5"
-        try:
-            if (type(eval(wordset[0]))!=int and type(eval(wordset[0]))!=float) or eval(wordset[0])<0:raise Exception
-            if (type(eval(wordset[1]))!=int and type(eval(wordset[1]))!=float) or eval(wordset[0])<0:raise Exception
-        except Exception:
-            print("sysinfo→"+msg("Single_Mode_Num_Error"))
-            return
-        else:
-            None
-        try:
-            inforaw=[line[3:line.index(":")],line[line.index(":")+1:-1]]
-            textsetcount=len(inforaw[0].split("/"))
-            if textsetcount>3:raise Exception
-            textset=inforaw[0].split("/")+[""]*(3-textsetcount)+[inforaw[1]]
-            if textset[0]=="":textset[0]="960"
-            if textset[1]=="":textset[1]="540"
-            if textset[2]=="":textset[2]="M"
-            if textset[3]=="":textset[3]=" "
-            if type(eval(textset[0]))!=int or eval(textset[0])<0:raise Exception
-            if type(eval(textset[1]))!=int or eval(textset[1])<0:raise Exception
-            if textset[2] not in "LMR":raise Exception
-        except Exception:
-            print("sysinfo→"+msg("Single_Mode_Num_Error"))
-            return
-        else:
-            None
-        print(msg("Freedom_Text_Setting_Info").format(textset[0],textset[1],textmode[textset[2]]))
-        print("\t\t\t",end="")
-        if wordset[0]!=0:
-            for i in textset[3]:
-                print(i,end="")
-                tm.sleep(eval(wordset[0]))
-        else:
-            print(textset[3],end="")
-        tm.sleep(eval(wordset[1]))
-    else : 
-        print("Sysinfo→"+msg("Single_Mode_Interpreter_Error").format(line[:-1]))
