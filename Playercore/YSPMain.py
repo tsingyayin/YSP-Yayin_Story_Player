1eb41851cf91dabe6af50da0f2db2a1552529666
JUNYI HE <77230422+tsingyayin@users.noreply.github.com>
2021-05-22 18:19:16 +0800

    Add files via upload

--- Playercore/YSPMain.py
+++ Playercore/YSPMain.py
@@ -7,16 +7,17 @@ sys.path.append(r"C:\Users\Administrator\source\repos\PlayerCore\PlayerCore\lang
 sys.path.append(r"C:\Users\Administrator\source\repos\PlayerCore\PlayerCore\Visual")
 from command.aaspcommand import *
 from langcontrol import *
-from Visual.ArtificialUI import *
+
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import platform
 
 #初始化语言
 langset(0)
 
-#第一指引
-print("sysinfo→"+msg("First_Print"))
+print("sysinfo→"+msg("System_Info"),platform.platform())
+
 
 if __name__=="__main__":
     DirectOpen=0
@@ -24,9 +25,34 @@ if __name__=="__main__":
         DirectOpen=1
         Usript="ui"
 
+
+#系统版本判断
+System_name=platform.platform().split("-")[0]
+System_VersionMain=platform.version().split(".")[0]+"."+platform.version().split(".")[1]
+System_VersionNum=platform.version().split(".")[2]
+
+
+if System_VersionMain!="10.0":
+    print(msg("System_Not_Win10").format(platform.version().split(".")[0]))
+    app=QApplication(sys.argv)
+        
+    sys.exit(app.exec_())
+
+
+if int(System_VersionNum)<16299 and System_VersionMain=="10.0":
+    print(msg("System_Win10_Too_Old").format(System_VersionNum))
+    app=QApplication(sys.argv)
+        
+    sys.exit(app.exec_())
+#第一指引
+print("sysinfo→"+msg("First_Print"))
+
 #主识别循环
 programme_end=0
 while programme_end==0 :
+
+   
+
     if DirectOpen!=1:
         Usript=input("Userinput→")
 
@@ -51,9 +77,14 @@ while programme_end==0 :
     elif Usript=="ui":
         if __name__=="__main__":
             app=QApplication(sys.argv)
-            Mainwindow=MainWindow()
-            Mainwindow.showFullScreen()
-            sys.exit(app.exec_())
+            monitor_range=ui()
+            if monitor_range[0]<1440 or monitor_range[1]<900:
+                print("sysinfo→"+msg("Screen_Too_Small").format(monitor_range[0],monitor_range[1]))
+                #break
+            else:
+                Mainwindow=MainWindow()
+                Mainwindow.showFullScreen()
+                sys.exit(app.exec_())
     elif Usript=="exit":
         break
     
